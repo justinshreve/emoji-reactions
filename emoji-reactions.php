44d1e8993081143d7b6f76f8496538a7fc361c20
Justin Shreve <justin.shreve@gmail.com>
2015-07-31 17:00:13 +0000

    Add plugin file with standard WordPress plugin header

---
+++ emoji-reactions.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Emoji Reactions
+ * Plugin URI: http://wordpress.org/plugins/emoji-reactions/
+ * Description: Give your readers more ways to provide feedback on your posts.
+ * Author: Justin Shreve
+ * Author URI: http://justin.gs
+ * Version: 1.0.0-pre
+ * License: GPLv2 or later
+ * Text Domain: emoji-reactions
+ * Domain Path: languages/
+ */
