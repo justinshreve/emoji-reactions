63f428d7b56138688c94878f9dc470793b82a48f
Justin Shreve <justin.shreve@gmail.com>
2015-08-08 04:43:55 +0000

    Comment and clean up some of the custom emoji admin code

--- includes/admin/custom-emoji.php
+++ includes/admin/custom-emoji.php
@@ -6,31 +6,46 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
+/**
+ * @todo esc_html__
+ * @todo rename all assets for this script to custom-emoji-admin.css/js
+ */
 class Emoji_Reactions_Custom_Emoji_Admin {
+
+	/**
+	 * Hook into WordPress.. alot
+	 */
 	public function __construct() {
+		// custom post type and related post type ui strings
 		add_action( 'init', array( $this, 'register_custom_emoji_cpt' ) );
+		add_filter( 'post_updated_messages', array( $this, 'updated_update_messages' ) );
+		add_filter( 'bulk_post_updated_messages', array( $this, 'bulk_post_updated_messages'), 10, 2 );
+
+		// hide and change various wp admin elements to make our admin experience nicer
 		add_action( 'admin_head', array( $this, 'move_featured_image_meta_box' ) );
 		add_filter( 'enter_title_here', array( $this, 'change_default_title' ) );
 		add_action( 'edit_form_before_permalink', array( $this, 'add_title_description' ) );
 		add_filter( 'admin_post_thumbnail_html', array( $this, 'add_image_description' ), 10, 2 );
 		add_filter( 'get_sample_permalink_html', array( $this, 'hide_permalink' ), -10, 4 );
 		add_filter( 'get_shortlink', array( $this, 'hide_permalink' ), -10, 4 );
-		add_action( 'admin_print_styles-post.php', array( $this, 'enqueue_admin_css' ) );
-		add_action( 'admin_print_styles-post-new.php', array( $this, 'enqueue_admin_css' ) );
-		add_action( 'admin_print_styles-edit.php', array( $this, 'enqueue_admin_css' ) );
-		add_action( 'admin_print_scripts-post.php', array( $this, 'enqueue_admin_js' ) );
-		add_action( 'admin_print_scripts-post-new.php', array( $this, 'enqueue_admin_js' ) );
-		add_action( 'admin_print_scripts-edit.php', array( $this, 'enqueue_admin_js' ) );
-		add_filter( 'post_updated_messages', array( $this, 'updated_update_messages' ) );
-		add_filter( 'bulk_post_updated_messages', array( $this, 'bulk_post_updated_messages'), 10, 2 );
 		add_filter( 'post_row_actions', array( $this, 'remove_action_links' ), 10, 2 );
 		add_filter( 'bulk_actions-edit-custom-emoji', array( $this, 'bulk_actions' ) );
 		add_filter( 'months_dropdown_results', array( $this, 'hide_date_dropdown') , 10, 2 );
-
 		add_filter( 'manage_custom-emoji_posts_columns', array( $this, 'columns' ) );
 		add_action( 'manage_custom-emoji_posts_custom_column', array( $this, 'render_columns' ), 2 );
+
+		// load all of our assets
+		$pages = array( 'post', 'post-new', 'edit' );
+		foreach ( $pages as $page ) {
+			add_action( 'admin_print_styles-' . $page . '.php', array( $this, 'enqueue_admin_css' ) );
+			add_action( 'admin_print_scripts-' . $page . '.php', array( $this, 'enqueue_admin_js' ) );
+		}
 	}
 
+	/**
+	 * Reports if the currently visible UI screen is a custom emoji management screen (so we do not overwrite other pages)
+	 * @return boolean
+	 */
 	private function is_custom_emoji_screen() {
 		$screen = get_current_screen();
 		if ( ! empty ( $screen ) && 'custom-emoji' === $screen->post_type ) {
@@ -49,6 +64,9 @@ class Emoji_Reactions_Custom_Emoji_Admin {
 		return false;
 	}
 
+	/**
+	 * Registers a custom post type for storing custom emoji
+	 */
 	public function register_custom_emoji_cpt() {
 			$labels = $this->get_labels();
 
@@ -73,9 +91,8 @@ class Emoji_Reactions_Custom_Emoji_Admin {
 	}
 
 	/**
-	 * Returns an array of labels to use in the WordPress admin UI
+	 * Returns an array of labels to use in the WordPress admin UI when managing custom emoji
 	 * @return array
-	 * @todo esc_html__
 	 */
 	private function get_labels() {
 		return array(
@@ -98,6 +115,9 @@ class Emoji_Reactions_Custom_Emoji_Admin {
 		);
 	}
 
+	/**
+	 * "Updated" Status Messages (after a custom emoji is saved/updated)
+	 */
 	public function updated_update_messages( $messages ) {
 		$post             = get_post();
 		$post_type        = get_post_type( $post );
@@ -125,6 +145,9 @@ class Emoji_Reactions_Custom_Emoji_Admin {
 		return $messages;
 	}
 
+	/**
+	 * Messages to show when custom emoji are updated via action links/bulk edit
+	 */
 	public function bulk_post_updated_messages( $bulk_messages, $bulk_counts ) {
 		$bulk_messages['custom-emoji'] = array(
 			'deleted'   => _n( '%s custom emoji permanently deleted.', '%s custom emoji permanently deleted.', $bulk_counts['deleted'], 'emoji-reactions' ),
@@ -134,8 +157,10 @@ class Emoji_Reactions_Custom_Emoji_Admin {
 		return $bulk_messages;
 	}
 
+	/**
+	 * Moves the "featured image" metabox to the main/"advanced" column and renames it to image
+	 */
 	public function move_featured_image_meta_box() {
-		// Bail if we are not on a custom emoji admin screen
 		if ( ! $this->is_custom_emoji_screen() ) {
 			return;
 		}
@@ -146,24 +171,31 @@ class Emoji_Reactions_Custom_Emoji_Admin {
 		}
 	}
 
+	/**
+	 * Renames the "title" box on the create new emoji screen to be more friendly
+	 */
 	public function change_default_title( $title ) {
 		if ( $this->is_custom_emoji_screen() ) {
-			$title = esc_html__( 'Choose a name' );
+			$title = esc_html__( 'Choose a name', 'emoji-reactions' );
 		}
 		return $title;
 	}
 
-	// we use edit_form_before_permalink instead of after_title because it displays closer to the input box
+	/**
+	 * Adds a helpful description under the "title" input field
+	 * we use edit_form_before_permalink instead of after_title because it displays closer to the input box
+	 */
 	public function add_title_description() {
-		// Bail if we are not on a custom emoji admin screen
 		if ( ! $this->is_custom_emoji_screen() ) {
 			return;
 		} ?>
-		<p class="description" id="custom-emoji-title-description"><?php esc_html_e( 'This is what you will enter to search for this emoji.', 'custom-emoji' ); ?></p><?php
+		<p class="description" id="custom-emoji-title-description"><?php esc_html_e( 'This is what you will enter to search for this emoji.', 'emoji-reactions' ); ?></p><?php
 	}
 
+	/**
+	 * Adds a helpful description under the "image" input area
+	 */
 	public function add_image_description( $content, $post_id ) {
-		// Bail if we are not on a custom emoji admin screen
 		if ( ! $this->is_custom_emoji_screen() ) {
 			return $content;
 		}
@@ -173,17 +205,21 @@ class Emoji_Reactions_Custom_Emoji_Admin {
 		return $content;
 	}
 
-	// Used to override two filters with 4 args
+	/**
+	 * Hides the permalink and shortlink areas
+	 * Both filters take 4 args so we will just wipe them out with one function
+	 */
 	public function hide_permalink( $return, $second, $third, $fourth ) {
-		// Bail if we are not on a custom emoji admin screen
 		if ( ! $this->is_custom_emoji_screen() ) {
 			return $return;
 		}
 		return "";
 	}
 
+	/**
+	 * Removes the quick edit and view action links from the list table
+	 */
 	public function remove_action_links( $actions ) {
-		// Bail if we are not on a custom emoji admin screen
 		if ( ! $this->is_custom_emoji_screen() ) {
 			return $actions;
 		}
@@ -193,8 +229,10 @@ class Emoji_Reactions_Custom_Emoji_Admin {
 	    return $actions;
 	}
 
+	/**
+	 * Removes the edit option from bulk actions, since we are turning off quick edit
+	 */
 	public function bulk_actions( $actions ) {
-		// Bail if we are not on a custom emoji admin screen
 		if ( ! $this->is_custom_emoji_screen() ) {
 			return $actions;
 		}
@@ -203,6 +241,9 @@ class Emoji_Reactions_Custom_Emoji_Admin {
 		return $actions;
 	}
 
+	/**
+	 * Hides the date dropdown filter
+	 */
 	public function hide_date_dropdown( $months, $post_type ) {
 		if ( 'custom-emoji' !== $post_type ) {
 			return $months;
@@ -210,41 +251,51 @@ class Emoji_Reactions_Custom_Emoji_Admin {
 		return array();
 	}
 
+	/**
+	 * Load's the admin CSS for our custom emoji screens
+	 */
 	public function enqueue_admin_css() {
-		// Bail if we are not on a custom emoji admin screen
 		if ( ! $this->is_custom_emoji_screen() ) {
 			return;
 		}
 
-		wp_enqueue_style( 'emoji-reactions-create-admin-css', plugins_url( 'assets/css/create-admin.css' , __FILE__ ) );
+		wp_enqueue_style( 'emoji-reactions-create-admin-css', plugins_url( 'assets/css/create-admin.css' , dirname ( dirname( __FILE__ ) ) ) );
 	}
 
+	/**
+	 * Load's the admin JS for our custom emoji screens
+	 */
 	public function enqueue_admin_js() {
-		// Bail if we are not on a custom emoji admin screen
 		if ( ! $this->is_custom_emoji_screen() ) {
 			return;
 		}
 
-		wp_enqueue_script( 'emoji-reactions-create-admin-js', plugins_url( 'assets/js/create-admin.js' , __FILE__ ) );
+		wp_enqueue_script( 'emoji-reactions-create-admin-js', plugins_url( 'assets/js/create-admin.js' , dirname ( dirname ( __FILE__ ) ) ) );
 		wp_localize_script( 'emoji-reactions-create-admin-js', 'emojiReactionsStrings', array(
-			'saveNewEmoji'  => esc_html__( 'Save New Emoji', 'emoji-reactions' ),
+			'saveNewEmoji'    => esc_html__( 'Save New Emoji', 'emoji-reactions' ),
 			'save'            => esc_html__( 'Save', 'emoji-reactions' ),
 		) );
 	}
 
+	/**
+	 * Reduce the list table to just a checkbox, image, and name
+	 */
 	public function columns( $existing_columns ) {
 		$columns          = array();
 		$columns['cb']    = '<input type="checkbox" />';
-		$columns['thumb'] = __( 'Image', 'emoji-reactions' );
+		$columns['image'] = __( 'Image', 'emoji-reactions' );
 		$columns['name']  = __( 'Name', 'emoji-reactions' );
 		return $columns;
 	}
 
+	/**
+	 * Define how the image and name fields are outputed
+	 */
 	public function render_columns( $column ) {
 		global $post;
 
 		switch ( $column ) {
-			case 'thumb' :
+			case 'image' :
 				echo '<a href="' . get_edit_post_link( $post->ID ) . '">' . the_post_thumbnail( array( 44, 44 ) ) . '</a>';
 				break;
 			case 'name' :
@@ -257,6 +308,7 @@ class Emoji_Reactions_Custom_Emoji_Admin {
 				break;
 		}
 	}
+
 }
 
 new Emoji_Reactions_Custom_Emoji_Admin;
